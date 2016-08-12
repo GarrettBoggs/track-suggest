0bbe3043fc91726208a8209f322c8dd901c239e2
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2016-08-12 08:44:37 -0700

    add second question and branching logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,46 @@
+var javaPoints = 0;
+var cPoints = 0;
+var RubyPoints = 0;
+var phpPoints = 0;
+var result = "";
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     var build = $("input:radio[name=build]:checked").val();
-    
 
+
+    if(build == "content")
+    {
+      phpPoints += 1;
+    }
+    else if(build == "games")
+    {
+      cPoints += 1;
+    }
+    else if(build == "apps")
+    {
+      RubyPoints += 1;
+    }
+    else // all
+    {
+      javaPoints += 1;
+    }
+
+
+    if((javaPoints > phpPoints) && (javaPoints > cPoints) && (javaPoints > RubyPoints))
+    {
+      result="Java!";
+    }
+    else if((javaPoints > phpPoints) && (javaPoints > cPoints) && (javaPoints > RubyPoints))
+    {
+      result="Php"
+    }
+    alert(phpPoints);
+
+    $(".track").text(result);
 
     $("#output").show();
+
     event.preventDefault();
   });
 });
